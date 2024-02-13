efbdfee53017f98cbbe8f9694add905d2bb6afe1
Dylan Wax <dylanwax@Dylans-Air.lan>
2024-02-12 21:02:25 -0500

    changes

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,77 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+  },
+
+  {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {},
+  },
+
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- For standard functions
+      "MunifTanjim/nui.nvim", -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = true,
+  },
+
+  {
+    'm-demare/attempt.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+  },
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  },
+
+  {
+    'MunifTanjim/nui.nvim'
+  },
+
+  {
+    'jghauser/mkdir.nvim'
+  },
+
+  {
+    "rcarriga/nvim-notify"
+  },
+
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+  },
+}
